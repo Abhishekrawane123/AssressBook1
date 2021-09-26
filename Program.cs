79b8c4b3a00d9d4d40bd9da62e69e219bb1d2c1e
pknowledge <abhishekrawane1195@gmail.com>
2021-09-26 16:43:54 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Address Book ");
+            AddressBook1 addressBook = new AddressBook1();
+            addressBook.acceptData();
+        }
+    }
+}
